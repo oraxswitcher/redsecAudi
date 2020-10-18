6133659888cda6c0453d1443af8e96b536348736
oraxswitcher <raouf200026@gmail.com>
2020-10-18 22:13:05 +0100

    first commit

---
+++ JS/app.js
@@ -0,0 +1,18 @@
+new Vue({
+    el: '#app',
+    data: {
+        show: false
+    }
+});
+new Vue({
+    el: '#app1',
+    data: {
+        on: false
+    }
+});
+new Vue({
+    el: '#app2',
+    data: {
+        onn: false
+    }
+});
---
+++ index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css2?family=Alegreya&family=Grenze+Gotisch:wght@300&display=swap" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css2?family=Abel&family=Lobster&display=swap" rel="stylesheet"> 
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">    	
+	<link rel="stylesheet" href="css/style.css">
+	<title>Audi | HOME</title>
+</head>
+<body>
+	<div class="full-page">
+		<header>
+			<div class="nav" >
+                <div class="logo">
+					<a href="http://localhost/redsec%20audi%20compi/">
+						<img src="./img/logo.gif" alt="logo">
+					</a>
+                </div>
+                <div class="nav-bar">
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Experience Audi</a>
+                    <a href="#">Company</a>
+                    <a href="#">Careers</a>
+                </div>
+			</div>
+			<div class="company-details">
+				<div class="bio">
+					<div class="logo2"><img src="./img/logo2.png" alt="logo2"></div>
+					<p>“Lead by technology”</p>
+				</div>
+			</div>
+		</header>
+		<div class="web-bio" >
+			<div id="app1" @mouseover="on=true" class="web-bio-cont">
+				<transition name="on1">
+					<h1 v-if="on">About us</h1>	
+				</transition>
+				<transition name="on2">
+					<P v-if="on">Audi AG (German: (About this soundlisten)) is a German automobile manufacturer that designs, engineers, produces, markets and distributes luxury vehicles. Audi is a member of the Volkswagen Group and has its roots at Ingolstadt, Bavaria, Germany. Audi-branded vehicles are produced in nine production facilities worldwide.
+						The origins of the company are complex, going back to the early 20th century and the initial enterprises (Horch and the Audiwerke) founded by engineer August Horch; and two other manufacturers (DKW and Wanderer), leading to the foundation of Auto Union in 1932. The modern era of Audi essentially began in the 1960s when Auto Union was acquired by Volkswagen from Daimler-Benz. After relaunching the Audi brand with the 1965 introduction of the Audi F103 series, Volkswagen merged Auto Union with NSU Motorenwerke in 1969, thus creating the present day form of the company.
+						The company name is based on the Latin translation of the surname of the founder, August Horch. "Horch", meaning "listen" in German, becomes "audi" in Latin. The four rings of the Audi logo each represent one of four car companies that banded together to create Audi's predecessor company, Auto Union. Audi's slogan is Vorsprung durch Technik, meaning "Being Ahead through Technology". However, Audi USA had used the slogan "Truth in Engineering" from 2007 to 2016, and have not used the slogan since 2016. Audi, along with fellow German marques BMW and Mercedes-Benz, is among the best-selling luxury automobile brands in the world.</P>
+				</transition>
+				<transition name="on3">
+					<button v-if="on">Discover More</button>
+				</transition>
+			</div>
+		</div>
+		<div id="app2" @mouseover="onn=true" class="world">
+			<h2 style="font-size: 50px;font-family: 'Lobster', cursive;">World of audi</h2>
+			<div class="world-audi">
+			<transition name="on4">	
+				<div v-if="onn" class="imgs1">
+					<div class="img1" style="background:url(./img/company.jpg);background-repeat: no-repeat;background-size: 650px 320px;margin-right: 4px;"><a href="#"><div class="back"><h1>Audi Company</h1></div></a></div>
+					<div class="img1" style="background:url(./img/career.jpg);background-repeat: no-repeat;background-size: 650px 320px;"><a href="#"><div class="back"><h1>Audi Careers</h1></div></a></div>
+				</div>
+			</transition>	
+			<transition name="on5">
+				<div v-if="onn" class="imgs1">
+					<div class="img1" style="background:url(./img/models.jpg);background-repeat: no-repeat;background-size: 650px 320px;margin-right: 4px;"><a href="#"><div class="back"><h1>Audi Models</h1></div></a></div>
+					<div class="img1" style="background:url(./img/experience.jpg);background-repeat: no-repeat;background-size: 650px 320px;"><a href="#"><div class="back"><h1>Audi experience</h1></div></a></div>
+				</div>
+			</transition>	
+			</div>
+		</div>
+		<section id="app">
+			<div  @mouseover="show=true" class="section-vue">
+				<div  class="text-top">
+					<div class="text-left">_____________________________________________________________________</div>
+					<div class="text">Cars Review</div>
+					<div class="text-right">_____________________________________________________________________</div>
+				</div>
+				<transition name="showup">
+					<div v-if="show" class="cars-container">
+						<div class="car"><img src="./img/audi-a1.jpg" alt="audi A1"></div>
+						<div class="car-content">
+							<h2>AUDI A1</h2>
+							<p>The Audi A1 (internally designated Typ 8X) is a supermini car launched by Audi at the 2010 Geneva Motor Show. Sales of the initial 3 door A1 model started in Germany in August 2010, with the United Kingdom following in November 2010. A five-door version, called Sportback, was launched in November 2011, with sales starting in export markets during spring 2012</p>
+							<p>Price:$19,500 – 59,850</p>
+						</div>
+					</div>
+				</transition>
+				<transition name="showup2">
+					<div v-if="show" class="cars-container">
+						<div class="car"><img src="./img/audi-a5.jpg" alt="audi a5"></div>
+						<div class="car-content">
+							<h2>AUDI A5</h2>
+							<p>The Audi A5 is a series of compact executive coupe cars produced by the German automobile manufacturer Audi since March 2007. The A5 range additionally comprises the coupe, cabriolet, and "Sportback" (a four-door hatchback with a fastback-like roofline) version of the Audi A4 saloon and estate models. </p>
+							<p>Price:$42,900 - 60,200</p>
+						</div>
+					</div>
+				</transition>
+				<transition name="showup3">
+					<div v-if="show" class="cars-container">
+						<div class="car"><img src="./img/audi-Q7.jpg" alt="audi q7"></div>
+						<div class="car-content">
+							<h2>AUDI Q7</h2>
+							<p>The Audi Q7 is a mid-size luxury SUV made by the German manufacturer Audi, unveiled in September 2005 at the Frankfurt Motor Show. Production of this seven-seater SUV began in the autumn of 2005 at the Volkswagen Bratislava Plant in Bratislava, Slovakia. It was the first SUV offering from Audi and went on sale in 2006. </p>
+							<p>Price:$54,950 - 85,000 </p>
+						</div>
+					</div>
+				</transition>
+				<transition name="showup4">
+					<div v-if="show" class="cars-container">
+						<div class="car"><img src="./img/audi-r8.jpg" alt="audi r8"></div>
+						<div class="car-content">
+							<h2>AUDI R8</h2>
+							<p>The Audi R8 is a mid-engine, 2-seater sports car, which uses Audi's trademark quattro permanent all-wheel drive system. It was introduced by the German car manufacturer Audi AG in 2006. </p>
+							<p>Price:$169,900 - 208,100</p>
+						</div>
+					</div>
+				</transition>
+			</div>
+		</section>
+	</div>
+	<footer>
+			<div class="social">
+				<a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-youtube"></i></a>
+                <a href="#"><i class="far fa-envelope"></i></a>
+                <a href="#"><i class="fab fa-linkedin"></i></a>
+			</div>
+			<div class="footer-back">
+				<div class="footer-infos">
+					<div class="footer-infos1">
+						<h2>Services</h2>
+						<a href="#">myAudi</a>
+						<a href="#">Press</a>
+						<a href="#">Contact</a>
+						<a href="#">Website Feedback</a>
+					</div>
+					<div class="footer-infos1">
+						<h2>Experience Audi</h2>
+						<a href="#">Models & Technology</a>
+						<a href="#">Mobility & Trends</a>
+						<a href="#">Inspiration & Lifestyle</a>
+						<a href="#">Audi Sport</a>
+					</div>
+					<div class="footer-infos1">
+						<h2>AUDI AG</h2>
+						<a href="#">Strategy</a>
+						<a href="#">Investor Relations</a>
+						<a href="#">Sustainability</a>
+						<a href="#">Career</a>
+					</div>
+				</div>
+			</div>
+			<div class="copyrights">
+				<div class="copyright">
+					<p>&copy;AUDI AG. All rights reserved</p>
+				</div>
+			</div>
+		</footer>
+		<script src="https://kit.fontawesome.com/fdddec0283.js" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+		<script src="./JS/app.js"></script>
+</body>
+</html>
